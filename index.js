86f5dc2231f670bb905c2da6604861cb8647a369
sheikh-momin <momin.emon14@gmail.com>
2023-04-07 02:08:17 -0700

    pushong

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin:"http://localhost:3000"
+  }
+));
 app.use(express.json());
 
 
