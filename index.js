dfc0463685a8a5074c8d1ab4d4bfc997278098a9
sheikh-momin <momin.emon14@gmail.com>
2023-04-07 03:06:52 -0700

    pushong

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-const app = express.Router();
+const app = express();
 
 // Middleware
 app.use(cors(
