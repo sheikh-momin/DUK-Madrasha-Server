9945e80d787fb17d9c19d5fd7580ee8bda337539
sheikh-momin <momin.emon14@gmail.com>
2023-04-06 23:51:16 -0700

    pushong

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: "http://localhost:3000/",
+    origin:"https://duk-madrasha.vercel.app/"
+  }
+));
 app.use(express.json());
 
 
